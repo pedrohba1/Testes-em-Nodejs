0d869b9e4d484455fc62bf91d581026eb8a0f169
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-20 13:56:38 -0300

    inicio de testes

--- server/src/database/migrations/20200420015635-create-recipient.js
+++ server/src/database/migrations/20200420015635-create-recipient.js
@@ -3,13 +3,7 @@ module.exports = {
     return queryInterface.createTable('users', { id: Sequelize.INTEGER });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
   },
 };

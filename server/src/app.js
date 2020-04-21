a119d8ea2abb3537404997b28ea61008b081c677
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-21 13:14:01 -0300

    variáveis de ambinete (não funciona direito)

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,4 @@
-import 'dotenv/config';
-
+import './bootstrap';
 import Youch from 'youch';
 import express from 'express';
 import 'express-async-errors';
@@ -29,7 +28,6 @@ class App {
     this.server.use(async (err, req, res, next) => {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
-
         return res.status(500).json(errors);
       }
 
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,12 +1,13 @@
-require('dotenv/config');
+require('../bootstrap');
 
 module.exports = {
-  dialect: 'postgres',
+  dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  storage: './__tests__/database.sqlite',
   define: {
     timestamps: true,
     underscored: true,
